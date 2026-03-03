1a8cfaddbbd590da9bef7f28a9788e34edd22a37
Hasibur Rahman Saju <98379969+saju1083@users.noreply.github.com>
2026-03-03 23:20:15 +0600

    Start.java

---
+++ Food/Start.java
@@ -0,0 +1,21 @@
+public class Start{
+
+public static void main (String args[]){
+
+   /* Food f1 = new Food();
+    f1.price=100;
+    f1.name="Veg Burger";
+    f1.showDetails();
+
+    Food f2= new Food(200, "Chicken Burger");
+    f2.showDetails();*/
+
+    Burger b1 = new Burger(2, 200, "Chicken Burger");
+    b1.showDetails();
+
+    Pizza p1 = new Pizza(12, 500, "Pepperoni Pizza");
+    p1.showDetails();
+
+}
+    
+}
