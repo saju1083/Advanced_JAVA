b72c40d188c3a5d2d1084ca9d632dd321680fa34
Hasibur Rahman Saju <98379969+saju1083@users.noreply.github.com>
2026-03-03 23:24:54 +0600

    Animal.java

---
+++ Animal/Animal.java
@@ -0,0 +1,13 @@
+//parent class/ base class/ super class
+public class Animal {
+
+    // behaviors/actions/ Java- method
+  void move(){
+    System.out.println("The animal is running");
+  }  
+
+}
+
+//inheritance - is a mechanism that allows a class to acquire
+// ->> properties and behaviors->> fields/variables & methods/functions
+// Benefit--  code reusability
