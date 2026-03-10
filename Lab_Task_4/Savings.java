17b1a1483db8b2e946f0d0c8819e6e6fb6e8c8ce
Hasibur Rahman Saju <98379969+saju1083@users.noreply.github.com>
2026-03-10 17:34:28 +0600

    Savings.java

---
+++ Lab_Task_4/Savings.java
@@ -0,0 +1,13 @@
+//SavingsAccount Class
+class SavingsAccount extends Account {
+
+    double interestRate;
+
+    void setInterestRate(double interestRate){
+        this.interestRate = interestRate;
+    }
+
+    double getInterestRate(){
+        return interestRate;
+    }
+}
